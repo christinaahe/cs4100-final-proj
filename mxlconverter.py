d100da4469ee7eafc41a18ffa9954efaac69ab5e
trowan7 <rowan.t@northeastern.edu>
2024-04-01 15:41:16 -0400

    Added pickle dump for chords

--- mxlconverter.py
+++ mxlconverter.py
@@ -1,5 +1,7 @@
 from music21 import *
 import os
+import numpy as np
+import pickle 
 
 def extract_notes_and_chords_by_measure(mxl_file_path):
     try:
@@ -28,23 +30,97 @@ def extract_notes_and_chords_by_measure(mxl_file_path):
     except converter.ConverterException as e:
         print(f"Error parsing MXL file: {e}")
         return None, None
+    
+# Remove extensions, inversions, and added tones from chords
+def remove_extensions(chords):
+    cleaned_chords = []
+    for chord in chords:
+        name = chord.figure
+        if not name == 'N.C.':
+            # Remove added tones
+            add_index = name.find('add')
+            if add_index != -1:
+                name = name[:add_index -1]
 
-folder_path = 'data'
+            # Remove inversions
+            inversion_index = name.find('/')
+            if inversion_index != -1:
+                name = name[:inversion_index]
 
+            # Remove 6th chords
+            six_index = name.find('6')
+            if six_index != -1:
+                name = name.replace('6', '7')
+            
+            # Remove 9th chords
+            nine_index = name.find('9') 
+            if nine_index != -1:
+                name = name.replace('9', '7')
+
+            cleaned_chords.append(name)
+
+
+    return cleaned_chords
+
+# Convert chords to integers that represent pitches
+def convert_to_int(chords):
+    int_pitches = []
+    for chord in chords:
+        h = harmony.ChordSymbol(chord)
+        int_pitches.append([p.midi for p in h.pitches])
+    return int_pitches
+
+folder_path = '/home/trowan2/CS4100/cs4100-final-proj/data'
+note_data = []
+chord_data = []
+file_names = np.array([])
+
+# Example usage
+# mxl_file_path = 'data/Afternoon_in_Paris.mxl'
 for filename in os.listdir(folder_path):
+
     # Construct the full file path
     file_path = os.path.join(folder_path, filename)
-# # Example usage
-# mxl_file_path = 'data/Afternoon_in_Paris.mxl'
-    print(file_path)
-    notes_by_measure, chords_by_measure = extract_notes_and_chords_by_measure(file_path)
-    print(notes_by_measure, chords_by_measure)
+    filename_clean = filename.replace('_', ' ').replace('.mxl', '')
 
-# if notes_by_measure and chords_by_measure:
+    file_names = np.append(file_names, filename_clean)
+    single_song_chords = []
+
+    notes_by_measure, chords_by_measure = extract_notes_and_chords_by_measure(file_path)
+
+    if notes_by_measure and chords_by_measure:
+        for measure_number, chords in chords_by_measure.items():
+            try: 
+                cleaned_chords = remove_extensions(chords)
+                int_pitches = convert_to_int(cleaned_chords)
+                single_song_chords.append(int_pitches)
+            except:
+                single_song_chords.append([[]])
+                #print("Failed to convert")
+    
+    chord_data.append(single_song_chords)
+
+    """
+    # Printing test
+    if notes_by_measure and chords_by_measure:
+
+
 #     print("Notes by Measure:")
 #     for measure_number, notes in notes_by_measure.items():
 #         print(f"Measure {measure_number}: {notes}")
-    
-#     print("\nChords by Measure:")
-#     for measure_number, chords in chords_by_measure.items():
-#         print(f"Measure {measure_number}: {chords}")
+        
+        #print("\nChords by Measure: ", filename_clean)
+        for measure_number, chords in chords_by_measure.items():
+            try: 
+                cleaned_chords = remove_extensions(chords)
+                int_pitches = convert_to_int(cleaned_chords)
+                #print(f"Measure {measure_number}: {[chord for chord in cleaned_chords]}")
+
+            except:
+                print("Failed to convert")
+    """
+
+with open('chord_data.pkl', 'wb') as f:
+    pickle.dump(chord_data, f)
+
+np.savetxt('song_names.txt', file_names, fmt='%s')
