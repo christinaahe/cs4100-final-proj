54770ec9154e34d3461685869a022cdf89399b06
ryandelano <rdelano6@gmail.com>
2024-04-02 16:14:16 -0400

    Refactor HiddenMarkovModel class constructor and methods

--- HMM_Class/HMM.py
+++ HMM_Class/HMM.py
@@ -1,7 +1,16 @@
 import numpy as np
 
+
 class HiddenMarkovModel:
-    def __init__(self, true_states, observable_states, transitions, emissions, start_probs, end_probs):
+    def __init__(
+        self,
+        true_states,
+        observable_states,
+        transitions,
+        emissions,
+        start_probs,
+        end_probs,
+    ):
         self.true_states = true_states
         self.observable_states = observable_states
         self.T = transitions
@@ -17,7 +26,12 @@ class HiddenMarkovModel:
                 if i == 0:
                     forward_vals[j, i] = self.start_probs[j] * self.E[j, i]
                 else:
-                    vals = np.array([forward_vals[k, i-1] * self.E[j, i] * self.T[k, j] for k in range(len(states))])
+                    vals = np.array(
+                        [
+                            forward_vals[k, i - 1] * self.E[j, i] * self.T[k, j]
+                            for k in range(len(self.true_states))
+                        ]
+                    )
                     forward_vals[j, i] = forward_vals[j, i] = np.sum(vals)
         end = np.multiply(forward_vals[:, -1], self.end_probs)
         end_val = np.sum(end)
@@ -26,14 +40,22 @@ class HiddenMarkovModel:
     def backward(self, sequence):
         backward_vals = np.zeros((len(self.true_states), len(sequence)))
         for i in range(1, len(sequence) + 1, -1):
-            for j in range(len(states)):
+            for j in range(len(self.true_states)):
                 # case for end of sequence
                 if i == 1:
                     backward_vals[j, -i] = self.end_probs[j]
                 else:
-                    vals = np.array([backward_vals[k, -i + 1] * self.E[k, -i + 1] * self.T[j, k] for k in range(len(self.true_states))])
+                    vals = np.array(
+                        [
+                            backward_vals[k, -i + 1] * self.E[k, -i + 1] * self.T[j, k]
+                            for k in range(len(self.true_states))
+                        ]
+                    )
                     backward_vals = np.sum(vals)
-        start_state = [backward_vals[i, 0] * self.E[i, sequence[0]] for i in range(len(self.true_states))]
+        start_state = [
+            backward_vals[i, 0] * self.E[i, sequence[0]]
+            for i in range(len(self.true_states))
+        ]
         start_state = np.multiply(start_state, self.start_probs)
         start_val = np.sum(start_state)
         return backward_vals, start_val
@@ -42,21 +64,28 @@ class HiddenMarkovModel:
         eta_probs = np.zeros((len(self.true_states), len(sequence)))
         for i in range(len(sequence)):
             for j in range(len(self.true_states)):
-                eta_probs[j, i] = (forward_probs[j, i] * backward_probs[j, i]) / forward_val
+                eta_probs[j, i] = (
+                    forward_probs[j, i] * backward_probs[j, i]
+                ) / forward_val
         return eta_probs
 
     def xi(self, forward_probs, backward_probs, forward_val, sequence):
-        xi_probs = np.zeroes((len(self.true_states, len(sequence)-1, len(self.true_states))))
+        xi_probs = np.zeros(
+            (len(self.true_states), len(sequence) - 1, len(self.true_states)))
         for i in range(len(sequence) - 1):
             for j in range(len(self.true_states)):
                 for k in range(len(self.true_states)):
-                    xi_probs[j, i, k] = (forward_probs[j, i] * backward_probs[k, i+1] * self.E[k, i+1]) / forward_val
+                    xi_probs[j, i, k] = (
+                        forward_probs[j, i]
+                        * backward_probs[k, i + 1]
+                        * self.E[k, i + 1]
+                    ) / forward_val
         return xi_probs
 
     def train(self, sequence):
         forward_probs, forward_val = self.forward(sequence)
         for _ in range(1000):
-            #forward_probs, forward_val = self.forward(sequence)
+            # forward_probs, forward_val = self.forward(sequence)
             backward_probs, backward_val = self.backward(sequence)
             eta_probs = self.eta(forward_probs, backward_probs, forward_val, sequence)
             xi_probs = self.xi(forward_probs, backward_probs, forward_val, sequence)
@@ -66,18 +95,28 @@ class HiddenMarkovModel:
             T = np.zeros(self.T.shape)
             for i in range(len(self.true_states)):
                 for j in range(len(self.true_states)):
-                    for k in range(len(sequence) - 1):
-                        T[i, j] += xi_probs[i, k, j]
-                    T_normalizer = np.sum(xi_probs[i, k_x, i_x] for k_x in range(len(sequence) - 1) for i_x in range(len(self.true_states)))
+                    xi_sum = np.array(
+                        [
+                            xi_probs[i, k, j]
+                            for k in range(len(sequence) - 1)
+                            for i_x in range(len(self.true_states))
+                        ]
+                    )
+                    T[i, j] = np.sum(xi_sum)
+                    T_normalizer = np.sum(xi_sum)
                     if T_normalizer != 0:
                         T[i, j] /= T_normalizer
                     else:
                         T[i, j] = 0
             for i in range(len(self.true_states)):
                 for j in range(len(self.observable_states)):
-                    idxs = [k for k in range(len(sequence)) if self.observable_states[i] == sequence[k]]
+                    idxs = [
+                        k
+                        for k in range(len(sequence))
+                        if self.observable_states[i] == sequence[k]
+                    ]
                     numerator = np.sum(eta_probs[i, idxs])
-                    denom = np.num(eta_probs[i, :])
+                    denom = np.sum(eta_probs[i, :])
                     if denom != 0:
                         E[i, j] = numerator / denom
                     else:
@@ -88,21 +127,46 @@ class HiddenMarkovModel:
             temp_forward_val = forward_val
             forward_probs, forward_val = self.forward(sequence)
             diff = np.abs(forward_val - temp_forward_val)
-            if diff <= .00001:
+            if diff <= 0.00001:
                 break
 
+    def predict_algorithm(self, obs):
+        # Step 2: Initialize Variables
+        predict_table = [
+            [0.0 for _ in range(len(self.true_states))] for _ in range(len(obs))
+        ]
+        backpointer = [
+            [0 for _ in range(len(self.true_states))] for _ in range(len(obs))
+        ]
 
+        # Step 3: Calculate Probabilities
+        for t in range(len(obs)):
+            for s in range(len(self.true_states)):
+                if t == 0:
+                    predict_table[t][s] = self.start_probs[s] * self.E[s][obs[t]]
+                else:
+                    max_prob = max(
+                        predict_table[t - 1][prev_s] * self.T[prev_s][s]
+                        for prev_s in range(len(self.true_states))
+                    )
+                    predict_table[t][s] = max_prob * self.E[s][obs[t]]
+                    backpointer[t][s] = max(
+                        range(len(self.true_states)),
+                        key=lambda prev_s: predict_table[t - 1][prev_s]
+                        * self.T[prev_s][s],
+                    )
 
+        # Step 4: Traceback and Find Best Path
+        best_path_prob = max(predict_table[-1])
+        best_path_pointer = max(
+            range(len(self.true_states)), key=lambda s: predict_table[-1][s]
+        )
+        best_path = [best_path_pointer]
+        for t in range(len(obs) - 1, 0, -1):
+            best_path.insert(0, backpointer[t][best_path[0]])
 
-
-
-
-
-
-states = [i for i in range(10)]
-start_state_probs = [1/10 for i in states]
-transition_probs = np.ones((10, 10)) / 10
-observations = [i for i in range(5)]
-emission_probs = np.ones((5, 10)) / 5
+        # Step 5: Return Best Path
+        return best_path
+    
 
 
